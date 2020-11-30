58c6f1643a843150107d954c552c0c1b3999e3b7
unknown <ryanhuuu@gmail.com>
2020-11-30 16:55:37 -0500

    Updates

---
+++ precache-manifest.35645bdc903453bf7522ec89a67b7203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728f2ba6f80571642b9f3070c4faf3de",
+    "url": "/react-sushi/index.html"
+  },
+  {
+    "revision": "97fac2f607e937147a9b",
+    "url": "/react-sushi/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b4ed3fe7189aa8747110",
+    "url": "/react-sushi/static/css/main.b4b32c0a.chunk.css"
+  },
+  {
+    "revision": "97fac2f607e937147a9b",
+    "url": "/react-sushi/static/js/2.5e704e29.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/react-sushi/static/js/2.5e704e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ed3fe7189aa8747110",
+    "url": "/react-sushi/static/js/main.95ae31bb.chunk.js"
+  },
+  {
+    "revision": "609f557f2a69b3da6a14",
+    "url": "/react-sushi/static/js/runtime-main.be6db34f.js"
+  }
+]);
